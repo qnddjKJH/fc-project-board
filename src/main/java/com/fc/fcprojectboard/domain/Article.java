a6e81526472387f01e413d8fb31d5562a2e10c24
JongHun Kim(boongDev) <qnddjKJH.dev@gmail.com>
2022-09-22 00:14:39 +0900

    #10-도메인 엔티티 변환
    1도메인 == 1테이블 매칭 설계

--- src/main/java/com/fc/fcprojectboard/domain/Article.java
+++ src/main/java/com/fc/fcprojectboard/domain/Article.java
@@ -1,15 +1,76 @@
 package com.fc.fcprojectboard.domain;
 
-import java.time.LocalDateTime;
+import com.fc.fcprojectboard.config.jpa.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
-public class Article {
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
+import javax.persistence.Table;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy"),
+})
+@Entity
+public class Article extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Setter
+    @Column(nullable = false)
     private String title;   // 제목
+
+    @Setter
+    @Column(nullable = false, length = 1000)
     private String content; // 본문
+
+    @Setter
     private String hashtag; // 해시태그
 
-    private LocalDateTime createAt; // 생성일
-    private LocalDateTime modifiedAt;   // 수정일
-    private String createdBy;   // 생성자
-    private String modifiedBy;  // 수정자
+    @ToString.Exclude
+    @OrderBy("id")
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
+    private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
+
+    private Article(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+
+    public static Article of(String title, String content, String hashtag) {
+        return new Article(title, content, hashtag);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article article)) return false;
+        return id.equals(article.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
